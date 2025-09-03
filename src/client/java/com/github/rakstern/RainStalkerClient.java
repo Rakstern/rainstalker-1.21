eb5af71bfb1f694b3fbd2dc3b097b6a9e52d0fdc
Adam S <adamspanner@btinternet.com>
2025-09-03 23:20:48 +0100

    Initial commit

---
+++ src/client/java/com/github/rakstern/RainStalkerClient.java
@@ -0,0 +1,10 @@
+package com.github.rakstern;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RainStalkerClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
