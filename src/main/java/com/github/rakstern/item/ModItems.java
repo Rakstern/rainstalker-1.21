3ad5b4debae7c8a8775e1afc31e386f53bddacfb
Adam S <adamspanner@btinternet.com>
2025-09-04 11:55:15 +0100

    Very first item, the condensed droplet

---
+++ src/main/java/com/github/rakstern/item/ModItems.java
@@ -0,0 +1,29 @@
+package com.github.rakstern.item;
+
+import com.github.rakstern.RainStalker;
+import net.fabricmc.fabric.api.itemgroup.v1.ItemGroupEvents;
+import net.minecraft.item.Item;
+import net.minecraft.item.ItemGroups;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import net.minecraft.util.Identifier;
+
+import java.util.function.Function;
+
+public class ModItems {
+
+    public static void initialize(){
+        ItemGroupEvents.modifyEntriesEvent(ItemGroups.INGREDIENTS)
+                .register((itemGroup) -> itemGroup.add(ModItems.CONDENSED_DROPLET));
+    }
+
+    public static final Item CONDENSED_DROPLET = register(new Item(new Item.Settings()),"condensed_droplet");
+
+    public static Item register(Item item, String id) {
+        // Create the identifier for the item.
+        Identifier itemID = Identifier.of(RainStalker.MOD_ID, id);
+
+        // Register and return!
+        return Registry.register(Registries.ITEM, itemID, item);
+    }
+}
